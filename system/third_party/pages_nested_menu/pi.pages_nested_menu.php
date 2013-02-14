9b6ee42fa2b22c368f03aad9da6f43e90e4cc565
Nathan Pitman <nathan@ninefour.co.uk>
2013-02-14 02:04:52 +0000

    Added some basic examples for usage.

--- system/third_party/pages_nested_menu/pi.pages_nested_menu.php
+++ system/third_party/pages_nested_menu/pi.pages_nested_menu.php
@@ -473,7 +473,18 @@ function usage()
 {
 ob_start(); 
 ?>
-This plugin creates a list of your Pages.
+This plugin creates a list of your Pages (created using the native Pages module). Basic syntax is as follows:
+
+{exp:pages_nested_menu root="/about/" include_ul="no" include_root="no" depth="1"}
+    <a href="{pnm_page_url}" title="{pnm_title}">{pnm_title}</a>
+{/exp:pages_nested_menu}
+
+Another example showing how to style the 'current' page:
+
+{exp:pages_nested_menu root="/about/" include_ul="no" include_root="no" depth="1"}
+    <a href="{pnm_page_url}" title="{pnm_title}" {if "{segment_2}" == "{pnm_url_title}"} class="current"{/if}>{pnm_title}</a>
+{/exp:pages_nested_menu}
+
 <?php
 $buffer = ob_get_contents();
 
